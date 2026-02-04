590bf4adcc7798d9223a004de0147be34e4f18f7
shafti-code <shafti.code@gmail.com>
2026-02-04 14:43:34 +0100

    Readme

---
+++ README.m
@@ -0,0 +1,5 @@
+simple project templating system, usage:
+```bash
+template $(template name) $(project name)
+```
+where template name is the template inside your filesystem in the ~/simple_templates/ directory and project name is the output directory this simple script will generate your new project into (relative)
